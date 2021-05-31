653bb45825618a1e38256a70812b32e7e7976b65
Matt Taylor <mattbtay@gmail.com>
2021-05-30 22:48:33 -0500

    init

---
+++ components/CookCard.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="374"
+    @click="viewItem"
+  >
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+      height="250"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+      
+    ></v-img>
+
+    <!-- <v-card-title>{{data.type}}</v-card-title> -->
+
+    <!-- <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0 pb-5"
+      >
+        <v-rating
+          :value="data.rating"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+        <div class="grey--text ms-4">
+          {{data.rating}} (1)
+        </div>
+      </v-row>
+
+    </v-card-text> -->
+
+
+    
+  </v-card>
+</template>
+
+<script>
+    export default {
+      props: {
+       data: {
+         required:true,
+         type:Object
+       }
+      },
+    data: () => ({
+      loading: false,
+      selection: 1,
+    }),
+
+    methods: {
+      reserve () {
+        this.loading = true
+        setTimeout(() => (this.loading = false), 2000)
+      },
+      addItem(){
+        console.log('do something')
+      },
+      viewItem($event){
+        // debugger
+        console.log(this.data.type)
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col sm="6" md="4" v-for="meat in meats" :key=meat.date>
+          <cook-card :data="meat" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        sheet: false,
+      tiles: [
+        { img: 'keep.png', title: 'Keep' },
+        { img: 'inbox.png', title: 'Inbox' },
+        { img: 'hangouts.png', title: 'Hangouts' },
+        { img: 'messenger.png', title: 'Messenger' },
+        { img: 'google.png', title: 'Google+' },
+      ],
+      }
+    },
+    async asyncData ( {$content} ){
+      const meats = await $content('cooks').fetch()
+      return {
+        meats
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
